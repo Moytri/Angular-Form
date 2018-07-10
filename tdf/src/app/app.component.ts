d8d7211c1ad76a7488f4952275e6d8eb9039af40
moytri <moytritopa@gmail.com>
2018-07-10 13:40:34 -0700

    add data from component class

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  topics = ['Angular', 'React', 'Vue'];
+
+  public myOption = "my-option";
 }
