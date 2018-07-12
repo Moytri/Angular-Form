e02f36a27decac417c0b0fd9d83c9332ea76f549
moytri <moytritopa@gmail.com>
2018-07-12 13:57:06 -0700

    onSubmit logic and inject EnrollmentService

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User} from './user';
+import { EnrollmentService } from './enrollment.service'
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,31 @@ import { User} from './user';
 })
 export class AppComponent {
   topics = ['Angular', 'React', 'Vue'];
+  topicHasError = true;
 
   public myOption = "my-option";
 
-  public userModel = new User("", "moytri@gmail.com", "7784952346", '', 'morning', false);
+  public userModel = new User("Moytri", "moytri@gmail.com", "7784952346", 'default', 'morning', false);
+
+  //Injected Service
+  construtor (private _enrollmentService: EnrollmentService) {
+
+  }
+  validateTopic(value) {
+  		if(value == 'default') {
+  			this.topicHasError = true;
+  		}
+  		else {
+  			this.topicHasError = false;
+  		}
+    }
+
+    onSubmit() {
+    	console.log(this.userModel);
+    	this._enrollmentService.enroll(this.userModel)
+    	.subscribe(
+    		data => console.log('Success!', data),
+    		error => console.log('Error!', error)
+    	)
+    }
 }
