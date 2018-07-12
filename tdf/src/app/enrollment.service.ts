9c32e32d53c9fee0f606a2355cb4f8d9f09de51d
moytri <moytritopa@gmail.com>
2018-07-12 15:17:10 -0700

    errorHandle and rsjx

--- tdf/src/app/enrollment.service.ts
+++ tdf/src/app/enrollment.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { User } from './user';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnrollmentService {
  
-  _url = '';
+  _url = 'http://localhost:3000/enroll';
   constructor(private _http: HttpClient) { 
   }
 
-  enroll(user: User) {
-  	 //returns a reponse as an Observable
-  	 return this._http.post<Any>(this._url, user);
-  } 
-}
+
+  enroll (user: User) {
+   //returns a reponse as an Observable
+    return this._http.post<any>(this._url, user)
+      .pipe(catchError(this.errorHandler))
+  }
+
+  errorHandler(error: HttpErrorResponse) {
+    return throwError(error)
+  }}
